06e0c160824d37b01ec62d3db257fb4f14b8a7d8
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-01-13 23:42:38 +0100

    Enabled experimental lua loader, moved keybindings to another file

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -16,77 +16,10 @@ in {
     extraLuaFiles = [
       lazygit_toggle
     ];
+    enableLuaLoader = true; #Experimental
     useSystemClipboard = true;
     lineNumberMode = "number";
-    keymaps = [
-      {
-        key = "<leader>f";
-        mode = "n";
-        silent = true;
-        action = "<CMD>Neotree toggle<CR>";
-      }
-      {
-        key = ";";
-        mode = "n";
-        silent = true;
-        action = ":";
-      }
-      {
-        key = "<leader>ff";
-        mode = "n";
-        silent = true;
-        action = "<CMD>Telescope find_files<CR>";
-      }
-      {
-        key = "<leader>fg";
-        mode = "n";
-        silent = true;
-        action = "<CMD>Telescope live_grep<CR>";
-      }
-      {
-        key = "<leader>gg";
-        mode = "n";
-        silent = true;
-        action = ''<CMD>lua term_lazygit_toggle()<CR>'';
-      }
-      {
-        key = "<C-h>";
-        mode = ["n" "t"];
-        silent = true;
-        action = "<CMD>wincmd h<CR>";
-      }
-      {
-        key = "<C-j>";
-        mode = ["n" "t"];
-        silent = true;
-        action = "<CMD>wincmd j<CR>";
-      }
-      {
-        key = "<C-k>";
-        mode = ["n" "t"];
-        silent = true;
-        action = "<CMD>wincmd k<CR>";
-      }
-      {
-        key = "<C-l>";
-        mode = ["n" "t"];
-        silent = true;
-        action = "<CMD>wincmd l<CR>";
-      }
-      {
-        key = "<C-t>";
-        mode = ["n" "t"];
-        silent = true;
-        action = "<CMD>ToggleTerm<CR>";
-      }
-
-      #{
-      # key = "<leader>p";
-      # mode = "n";
-      # silent = true;
-      # action = '':lua require("cellular-automaton").start_animation("make_it_rain")<CR>'';
-      #}
-    ];
+    keymaps = import ./keybinds.nix;
     languages = {
       enableFormat = true;
       enableLSP = true;
@@ -164,7 +97,7 @@ in {
     };
 
     tabline = {
-      nvimBufferline.enable = true;
+      nvimBufferline.enable = false;
     };
 
     treesitter.context.enable = true;
